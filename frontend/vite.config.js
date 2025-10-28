095892416ff6993ddf90a60029aed9beb11ba447
KaiqueWalkerRodrigues <108485267+KaiqueWalkerRodrigues@users.noreply.github.com>
2025-10-28 12:42:50 -0300

    tailwindcss and name of vite-project

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwind from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwind()],
+  server: {
+    host: true,
+    port: 80,
+    watch: { usePolling: true },
+  },
+});
